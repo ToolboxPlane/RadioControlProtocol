eaab2592181cdc9a3b2649a6c097638f2412bf03
Paul Nykiel <paul.nykiel@gmail.com>
2017-11-05 11:58:24 +0100

    Includes

--- rcLib.hpp
+++ rcLib.hpp
@@ -8,12 +8,8 @@
 #define  RC_LIB_END 0x93 //147_{10}
 #define DATA_BUFFER_SIZE 64
 
-// @TODO
-#ifdef __linux__
-    #include <cstdint>
-#else
-    #include "Arduino.h"
-#endif
+#include <stdint.h>
+
 
 namespace rcLib{
     class Package{
